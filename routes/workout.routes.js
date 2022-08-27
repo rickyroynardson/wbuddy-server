14107e75d17e241fe27b4acfec414c7760a8eaa0
Ricky Roy <rickyroynardson@gmail.com>
2022-08-27 23:26:31 +0700

    add middleware and path /workout

--- routes/workout.routes.js
+++ routes/workout.routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { getWorkouts } = require("../controllers/workout.controller");
+const requireAuth = require("../middleware/requireAuth");
 
 const router = express.Router();
+
+router.use(requireAuth);
 
 router.get("/", getWorkouts);
 
