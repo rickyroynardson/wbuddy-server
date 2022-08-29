c38afa48a797c6225baf698d4ebaebda5067cc7e
Ricky Roy <rickyroynardson@gmail.com>
2022-08-29 20:07:03 +0700

    install cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const userRoutes = require("./routes/user.routes");
 const workoutRoutes = require("./routes/workout.routes");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
